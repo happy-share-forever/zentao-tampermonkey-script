dc7b023c6a06fa37d9df467fafd44566a0b6fdab
Cong <gucovip@gmail.com>
2023-02-14 16:27:18 +0800

    feat: 支持发布到主仓库 (#6)

---
+++ .publish/config.js
@@ -0,0 +1,8 @@
+export default {
+  remote: 'https://github.com/happy-share-forever/tampermonkey-script.git',
+  repositoryName: 'tampermonkey-script',
+  branch: 'main',
+  distPath: 'dist',
+  publicPath: 'tampermonkey-script/ZenTao',
+  fileName: 'ZenTao.user.js'
+}
