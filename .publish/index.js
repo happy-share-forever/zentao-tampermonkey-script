a57009e77293f317837e5e8e1f8ced86793ca46c
Ziyuan Han <hanziyuan08@outlook.com>
2023-02-22 15:22:14 +0800

    feat: Replace `rm -rf` with `rimraf`

--- .publish/index.js
+++ .publish/index.js
@@ -4,21 +4,18 @@ import { execSync } from 'node:child_process'
 import fse from 'fs-extra'
 import config from './config.js'
 import pkg from '../package.json' assert { type: 'json' }
+import { rimraf } from 'rimraf';
 //
 const rootPath = path.resolve(path.join('.', '.publish'))
-const workspace = path.join(rootPath, 'workspace')
+const workspace = path.resolve(path.join(rootPath, 'workspace'))
 const distFilePath = path.resolve(path.join('.', config.distPath, config.fileName))
 const targetFilePath = path.join(workspace, ...config.publicPath.split('/'), config.fileName)
 const branchName = `publish/${Date.now()}`
 
+rimraf.sync(workspace)
 fse.ensureDirSync(workspace)
-execSync(`rm -rf ${workspace}${path.sep}* \\
-&& cd ${workspace} \\
-&& git clone ${config.remote} \\
-&& cd ${config.repositoryName} \\
-&& git checkout -b ${branchName} \\
-&& rm -rf ${targetFilePath}
-`)
+execSync(`cd ${workspace} && git clone ${config.remote} && cd ${config.repositoryName} && git checkout -b ${branchName}`)
+rimraf.sync(targetFilePath)
 
 fse.copySync(distFilePath, targetFilePath, {})
 const git = simpleGit(path.join(workspace, config.repositoryName))
