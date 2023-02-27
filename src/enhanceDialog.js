64a5c0df7cb4a42168e587164970abfbfd0b69ee
Cong <gucovip@gmail.com>
2023-02-27 17:19:07 +0800

    build: update version. (#19)

--- src/enhanceDialog.js
+++ src/enhanceDialog.js
@@ -1,5 +1,5 @@
 import { enhanceHistoryList } from './enhanceHistoryList.js'
-import {Context} from './class/Context.js'
+import { Context } from './class/Context.js'
 
 export function enhanceDialog (mutations, ctx) {
   const document = ctx.document
